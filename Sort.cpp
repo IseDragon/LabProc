0b8f31b759c45ea9660622f5aea981dcb178f725
IseDragon <tourer-ss@yandex.ru>
2018-04-22 15:03:02 +0700

    Add sort

---
+++ Sort.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include "Container.h"
+
+using namespace std;
+
+bool Compare(film* f1, film* f2);
+
+void Sort(container &c)
+{
+	for (int k = 0; k<c.n; k++)
+	{
+		int koli = 0;
+		node* prev = NULL;
+		node* Node = c.mas[k];
+		while (Node)
+		{
+			Node = Node->next;
+			koli++;
+		}
+		for (int i = 0; i < koli - 1; i++)
+		{
+			node* prev = NULL;
+			Node = c.mas[k];
+			for (int j = i + 1; j < koli; j++)
+			{
+				if (Compare(Node->f, Node->next->f))
+				{
+					node* next = Node->next;
+					Node->next = Node->next->next;
+					next->next = Node;
+					if (Node == c.mas[k])
+					{
+						c.mas[k] = next;
+						prev = next;
+					}
+					else
+					{
+						prev->next = next;
+						prev = next;
+					}
+				}
+				else
+				{
+					prev = Node;
+					Node = Node->next;
+				}
+			}
+		}
+	}
+}
--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ void Init(container &c);
 void Clear(container &c);
 void In(container &c, ifstream & ifst);
 void Out(container c, ofstream &ofst);
+void Sort(container &c);
 
 int main(int argc, char* argv[]) {
 
@@ -32,6 +33,7 @@ int main(int argc, char* argv[]) {
 
 	Clear(c);
 	ofst << "Empty container. " << endl;
+	Sort(c);
 	Out(c, ofst);
 
 	cout << "Stop" << endl;
